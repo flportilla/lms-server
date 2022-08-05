9d9c1e8e86e3ce56762f2c6b4a2f23ebcafa1567
Fabian Portilla <fas_456@hotmail.com>
2022-08-05 18:19:03 -0500

    Create an answer model

---
+++ models/answer.js
@@ -0,0 +1,17 @@
+const { model, Schema } = require('mongoose')
+
+const AnswerSchema = new Schema({
+    answer: {
+        type: String,
+        required: true
+    },
+
+})
+
+AnswerSchema.methods.toJSON = function () {
+    const { __v, password, _id, ...answer } = this.toObject();
+    answer.id = _id
+    return answer
+}
+
+module.exports = model('Answer', AnswerSchema)

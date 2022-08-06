3baa5f6108956910f3c3494967c70ccfcb16b1c5
Fabian Portilla <fas_456@hotmail.com>
2022-08-06 10:30:54 -0500

    Update question controller to send the rigth data

--- controllers/question.js
+++ controllers/question.js
@@ -5,32 +5,27 @@ const addQuestion = async (req, res) => {
 
     const { answer, opt1, opt2, opt3, opt4, statement } = req.body
 
-    //Create data for model
-    const newAnswerData = {
-        answer,
-    }
-    const newAnswer = new Answer(newAnswerData)
+    const newAnswer = new Answer({ answer })
 
     const newQuestionData = {
         statement,
         opt1, opt2, opt3, opt4,
         user: req.user.id,
-        answer: newAnswer.id
     }
 
     const newQuestion = new Question(newQuestionData)
+    newQuestion.answer = newQuestion.answer.concat(newAnswer.id)
+
     const [savedQuestion, savedAnswer] = await Promise.all([
         newQuestion.save(),
         newAnswer.save(),
     ]);
 
     res.json({
-        savedQuestion,
-        savedAnswer
+        msg: savedQuestion
     })
 }
 
-//TODO: Show questions with answer populated
 
 module.exports = {
     addQuestion
